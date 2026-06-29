6f70fd049cdfc80fa253d6d2b1deac47a12bac73
Praveena <praveenasenthilkumar340@gmail.com>
2026-06-29 14:53:32 +0530

    Binary tree implemented

---
+++ binary_tree.java
@@ -0,0 +1,58 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class binary_tree {
+
+    Node root;
+
+    void inorder(Node node) {
+        if (node != null) {
+            inorder(node.left);
+            System.out.print(node.data + " ");
+            inorder(node.right);
+        }
+    }
+
+    void preorder(Node node) {
+        if (node != null) {
+            System.out.print(node.data + " ");
+            preorder(node.left);
+            preorder(node.right);
+        }
+    }
+
+    void postorder(Node node) {
+        if (node != null) {
+            postorder(node.left);
+            postorder(node.right);
+            System.out.print(node.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        binary_tree tree = new binary_tree();
+
+        tree.root = new Node(10);
+        tree.root.left = new Node(20);
+        tree.root.right = new Node(30);
+        tree.root.left.left = new Node(40);
+        tree.root.left.right = new Node(50);
+
+        System.out.print("Inorder Traversal: ");
+        tree.inorder(tree.root);
+
+        System.out.print("\nPreorder Traversal: ");
+        tree.preorder(tree.root);
+
+        System.out.print("\nPostorder Traversal: ");
+        tree.postorder(tree.root);
+    }
+}
